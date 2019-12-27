9fb0aed61b2a081bd135dd80abe872a6ee2e254a
LEEEUNSOUL <54984902+LEEEUNSOUL@users.noreply.github.com>
2019-12-27 15:05:56 +0900

    오후 3시에 수정했습니다.

--- playGround.cpp
+++ playGround.cpp
@@ -4,10 +4,11 @@
 //초기화는 여기다 해라!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 HRESULT playGround::Init()
 {
-	gameNode::Init(true); // 불~~~~~~~편
+	gameNode::Init(true); 
+	
+	// 불~~~~~~~편
 	
 	//맛점_은솔 --> 오늘의 메뉴는 수유리 우동집입니다. 
-
 
 	return S_OK;
 }
