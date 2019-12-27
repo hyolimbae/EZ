9bac3c6c6cd06b6cd1cf4295c905c00d56d69940
GgaetIp <skyoung0204@naver.com>
2019-12-27 12:39:30 +0900

    first commit

--- playGround.cpp
+++ playGround.cpp
@@ -1,21 +1,10 @@
 #include "stdafx.h"
 #include "playGround.h"
 
-
-playGround::playGround()
-{
-}
-
-
-playGround::~playGround()
-{
-}
-
 //초기화는 여기다 해라!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 HRESULT playGround::Init()
 {
-	gameNode::Init(true);
-
+	gameNode::Init(true); // 불~~~~~~~편
 	
 
 
--- playGround.h
+++ playGround.h
@@ -7,9 +7,6 @@ using namespace std;
 class playGround : public gameNode
 {
 public:
-	playGround();
-	~playGround();
-
 	virtual HRESULT Init() override;		
 
 
